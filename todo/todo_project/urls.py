4cfcb1bd3b95f4a55fbdaa2db08d08f1b4b3df94
MederbekTuratbekov <142796921+MederbekTuratbekov@users.noreply.github.com>
2026-08-05 02:37:43 +0600

    ...

--- todo/todo_project/urls.py
+++ todo/todo_project/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('todo_app.urls')),
     path('accounts/', include('allauth.urls')),
